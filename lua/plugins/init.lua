7fc9768eb8a2d7a1745d89ecdf39d9f9c0f5ef8c
Fernando Fuentes <fernandocastillo.8@hotmail.com>
2025-06-01 14:58:17 -0600

    Installed lazy git successfully

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -25,4 +25,22 @@ return {
   		},
   	},
   },
+
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<CR>", desc = "LazyGit UI"}
+        }
+    }
 }
