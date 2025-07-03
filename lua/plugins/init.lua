84aa7bac810539e1dde1cd6ffdc0dfc2e588c917
Fernando Fuentes <fernandocastillo.8@hotmail.com>
2025-07-03 11:34:30 -0600

    Installation of the Windsurf plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -70,4 +70,15 @@ return {
             })
     end,
   },
+
+    {
+        'Exafunction/windsurf.vim',
+        event = 'BufEnter',
+        config = function ()
+            vim.keymap.set('i', '<Tab>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+            vim.keymap.set('i', '<C-;>', function () return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+            vim.keymap.set('i', '<C-,>', function () return vim.fn['codeium#CycleCompletions']() end, { expr = true, silent = true })
+            vim.keymap.set('i', '<C-x>', function () return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+        end
+    },
 }
