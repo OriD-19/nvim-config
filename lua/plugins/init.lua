ed731049ed8f0ab8eec5e4549688febbb0b77570
Fernando Fuentes <fernandocastillo.8@hotmail.com>
2026-01-25 17:37:48 -0600

    adding oil nvim for better file navigation (bye tree view)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -137,4 +137,16 @@ return {
       vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement under cursor", noremap = true })
     end,
   },
+
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
 }
